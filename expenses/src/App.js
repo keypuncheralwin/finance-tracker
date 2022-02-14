eaa0074bfbec7ef1e223f6c027260dbe42428ac5
Alwin George <alwingeorge11@gmail.com>
2022-02-14 17:56:03 +1030

    add basic routing

--- expenses/src/App.js
+++ expenses/src/App.js
@@ -1,9 +1,27 @@
 import './App.css'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+
+//Pages and components
+import Home from './pages/home/Home'
+import Signup from './pages/signup/Signup'
+import Login from './pages/login/Login'
 
 function App() {
   return (
     <div className="App">
-
+      <BrowserRouter>
+        <Switch>
+          <Route exact path='/'>
+            <Home />
+          </Route>
+          <Route path='/login'>
+            <Login />
+          </Route>
+          <Route path='/signup'>
+            <Signup />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
