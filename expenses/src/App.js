20f5303a01b5dfff5b035e4e88d8f2f7df95f94f
Alwin George <alwingeorge11@gmail.com>
2022-02-14 18:09:15 +1030

    add navbar

--- expenses/src/App.js
+++ expenses/src/App.js
@@ -5,11 +5,13 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import Home from './pages/home/Home'
 import Signup from './pages/signup/Signup'
 import Login from './pages/login/Login'
+import Navbar from './components/Navbar'
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Navbar />
         <Switch>
           <Route exact path='/'>
             <Home />
